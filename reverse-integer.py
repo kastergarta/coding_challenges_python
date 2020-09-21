b2562598e752d9fb235dbff00c0393c41963d9ef
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-09-20 22:22:41 -0400

    reverse integer

---
+++ reverse-integer.py
@@ -0,0 +1,11 @@
+def reverse(self, x):
+        res, isPos = 0, 1
+        if x < 0:
+            isPos = -1
+            x = -1 * x
+        while x != 0:
+            res = res * 10 + x % 10
+            if res > 2147483647:
+                return 0
+            x /= 10
+        return res * isPos
